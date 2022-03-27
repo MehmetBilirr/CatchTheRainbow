bc5a7a7804ddc6f7a295c34b67a48cab66d3de9b
MehmetBilirr <mehmetbilir@aol.com>
2022-03-27 22:02:50 +0300

    Define main.storyboard variables

--- CatchTheRainbow/ViewController.swift
+++ CatchTheRainbow/ViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    //Define Main.storyboard variables.
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var rainbow1: UIImageView!
+    @IBOutlet weak var rainbow2: UIImageView!
+    @IBOutlet weak var rainbow3: UIImageView!
+    @IBOutlet weak var rainbow4: UIImageView!
+    @IBOutlet weak var rainbow5: UIImageView!
+    @IBOutlet weak var rainbow6: UIImageView!
+    @IBOutlet weak var rainbow7: UIImageView!
+    @IBOutlet weak var rainbow8: UIImageView!
+    @IBOutlet weak var rainbow9: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
